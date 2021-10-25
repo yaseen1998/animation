496464be9b0eeae2d29775685398844c0feb8ea1
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-10-25 16:31:25 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import {
   Route,
 } from "react-router-dom";
 // import Gsap3 from './components/Gsap3'
-// import Gsap4 from './components/Gsap4'
+import Gsap4 from './components/Gsap4'
 // import Gsap5 from './components/Gsap5'
 // import Gsap1 from './components/Gsap1'
 import Gsap6 from './components/Gsap6/Gsap6'
-// import Gsap7 from './components/Gsap7/Gsap7'
+import Gsap7 from './components/Gsap7/Gsap7'
 import Gsap8 from './components/Gsap8/Gsap8'
 import Gsap9 from './components/Gsap9/Gsap9'
 import Gsap10 from './components/Gsap10/Gsap10' 
 import Gsap11 from './components/Gsap11/Gsap11' 
-// import Gsap12 from './components/Gsap12/Gsap12' 
+import Gsap12 from './components/Gsap12/Gsap12' 
 import Gsap13 from './components/Gsap13/Gsap13' 
 export class App extends Component {
   render() {
@@ -29,9 +29,9 @@ export class App extends Component {
           <Route path="/Gsap8">
             <Gsap8 />
           </Route>
-          {/* <Route path="/Gsap7">
+          <Route path="/Gsap7">
             <Gsap7/>
-          </Route> */}
+          </Route>
           <Route path="/Gsap6">
             <Gsap6/>
           </Route>
@@ -41,11 +41,14 @@ export class App extends Component {
           <Route path="/Gsap11">
             <Gsap11/>
           </Route>
-          {/* <Route path="/Gsap12">
+          <Route path="/Gsap12">
             <Gsap12/>
-          </Route> */}
+          </Route>
           <Route path="/Gsap13">
             <Gsap13/>
+          </Route>
+          <Route path="/Gsap4">
+            <Gsap4/>
           </Route>
         </Switch>
       </div>
--- src/components/Gsap13/Gsap13.js
+++ src/components/Gsap13/Gsap13.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { TweenMax, Power4, TimelineLite, Expo } from "gsap";
+import { TweenMax, Power3, TimelineMax, Expo,TimelineLite } from "gsap";
 import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
 import { FaFacebookF,FaTwitter ,FaInstagram} from "react-icons/fa";
@@ -7,12 +7,105 @@ import "./Gsap13.scss"
 
 
 const Gsap13 = () => {
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     let timeline1 = new TimelineMax();
+    //     timeline1
+    //         .from('.navbar13', 1.5, {
+    //             y: 30,
+    //             ease: Power3.easeInOut
+    //         })
+    //     })
+    
+    useEffect(()=>{
+        let nav = document.getElementById('bottom-nav13');
+        let home = document.getElementById('home13');
+
+        TweenMax.from('.title13',1.5,{
+            delay:0.2,
+            y:200,
+            ease:Power3.easeInOut
+        })
+        TweenMax.from('.navbar13',1,{
+            delay:0.2,
+            y:30,
+            ease:Power3.easeInOut,
+            autoAlpha:0
+            
+        },'-=1')
+        TweenMax.from('.sub_title13',1,{
+            delay:0.8,
+            y:30,
+            ease:Power3.easeInOut,
+            autoAlpha:0
+            
+        })
+        TweenMax.from('.bottom_nav13',2,{
+            width:"0%",
+            ease:Power3.easeInOut,
+            autoAlpha:0
+            
+        },'-=2')
+        nav.addEventListener('click', () => {
+            console.log('you click me');
+            let timeline2 = new TimelineMax()
+        timeline2
+            .set('.grid13', {
+                display: 'grid',
+            })
+            .to('.forest13 img', 3, {
+                y: -400,
+                scale: 1.1,
+                ease: Expo.easeInOut
+            })
+            .to('.bg-image13', 3, {
+                y: -600,
+                ease: Expo.easeInOut
+            }, '-=3')
+            .to('.sub_title13', 3, {
+                autoAlpha: 0,
+                y: -100,
+                ease: Expo.easeInOut
+            }, '-=3')
+            .to('.bottom_nav13', 3, {
+                width: '0%',
+                ease: Expo.easeInOut
+            }, '-=3')
+            .to('.navbar13', 3, {
+                color: '#fff',
+            }, '-=3')
+            .to('.imgs13', 2, {
+                height: '100%',
+                ease: Expo.easeInOut
+            }, '-=2')
+            .to('.imgs-213', 2, {
+                height: '100%',
+                ease: Expo.easeInOut
+            }, '-=1.8')
+            .to('.imgs-313', 2, {
+                height: '100%',
+                ease: Expo.easeInOut
+            }, '-=1.7')
+            .to('.imgs-413', 2, {
+                height: '100%',
+                ease: Expo.easeInOut
+            }, '-=1.6')
+            .to('.destination13', 2, {
+                // autoAlpha: 1,
+                y: -20,
+                ease: Expo.easeOut
+            }, '-=1.6');
+            home.addEventListener('click', ()=>{
+                timeline2.reverse();
+            })
+        })
+            
+    })
     return (
         <section className="Gsap13">
             <nav className='navbar13'>
                 <div className="left13">
                     <ul>
-                        <li id="home"><AiOutlineHome/> Home</li>
+                        <li id="home13"><AiOutlineHome/> Home</li>
                     </ul>
                 </div>
                 <div className="middle13">
@@ -35,7 +128,7 @@ const Gsap13 = () => {
                 </div>
                 <div className="desc13">
                     <p className="sub_title13">We challenged our friends around the globe to show us their destination unknowns. Share yours for a chance of winning your dream destination unknown trip.</p>
-                    <div className="bottom_nav13" id="bottom-nav">
+                    <div className="bottom_nav13" id="bottom-nav13">
                         <h4>EXPLORE DESTINATION</h4>
                         <div className="circle13"></div>
                     </div>
